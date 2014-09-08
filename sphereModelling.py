d5602f8a2475b124d3c6b819fc313417ea57f229
Supin P Surendran <supinps@gmail.com>
2014-09-08 08:33:07 +0530

    ADD: File Output Interface, Sphere Modelling
    Signed-off-by: Supin P Surendran <supinps@gmail.com>

--- sphereModelling.py
+++ sphereModelling.py
@@ -22,20 +22,35 @@
 
 import numpy
 from math import *
+from visual import *
 
 def pol2cart(R,r):
     th_step=(2*r)/(R+r)
-    theta=numpy.arange(0,360,th_step)
+    theta=numpy.arange(0,2*pi,th_step)
     phi=theta
     x=[]
     y=[]
     z=[]
     for i in theta:
         for j in phi:
+            print i,j
             x.append(round(R*sin(i)*cos(j),5))
             y.append(round(R*sin(i)*sin(j),5))
             z.append(round(R*cos(j),5))
     return x,y,z
+
+def write2file(x,y,z,r):
+    fout=open("coordinates.txt",'w')
+    ctr=0
+    for i,j,k in zip(x,y,z):
+        ctr=ctr+1
+        fout.write("solid main%d = sphere(%d, %d, %d,  %d)_maxh=3.0 ;\n" % (ctr, i, j, k, r))
+    fout.close()
+
+def plotSpheres(x,y,z,r,R):
+    sphere(pos=(0,0,0),radius=R,color=color.green)
+    for i,j,k in zip(x,y,z):
+        sphere(pos=(i,j,k),radius=r,color=color.red)
 
 def main():
     R=float(raw_input('Radius(Main Sphere) : '))
@@ -43,6 +58,8 @@ def main():
     x,y,z=pol2cart(R,r)
     for i,j,k in zip(x,y,z):
         print i,j,k
+    write2file(x,y,z,r)
+    plotSpheres(x,y,z,r,R)
 
 
 if __name__ == '__main__':
